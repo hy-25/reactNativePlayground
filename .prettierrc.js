4c76ca30c4cfee303ed1f156acb8c0ea66d76081
Hemant Yadv <hemantyadavtech25@gmail.com>
2024-04-07 22:07:59 +0530

    chore: added the basic setup

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,10 @@
 module.exports = {
   arrowParens: 'avoid',
   bracketSameLine: true,
-  bracketSpacing: false,
+  // bracketSpacing: true,
   singleQuote: true,
-  trailingComma: 'all',
-};
+  trailingComma: 'none',
+  tabWidth: 2,
+  semi: false,
+  printWidth: 120
+}
